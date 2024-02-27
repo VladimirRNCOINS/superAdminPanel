fe6e53a7d6e5feea4369b38a3e13ef7502c1059c
VladimirRNCOINS <v.shabalov@rncoins.ru>
2024-02-27 13:56:51 +0300

    закомментировал  в моделе User

--- app/Models/User.php
+++ app/Models/User.php
@@ -19,11 +19,11 @@ class User extends Authenticatable
      *
      * @var array<int, string>
      */
-    protected $fillable = [
+    /*protected $fillable = [
         'name',
         'email',
         'password',
-    ];
+    ];*/
 
     protected $guarded = ['*'];
 
